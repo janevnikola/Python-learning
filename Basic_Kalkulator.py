b189b16343dd391e7ef8555d7f5a13e83de32e48
janevnikola <72938546+janevnikola@users.noreply.github.com>
2021-10-23 16:41:01 +0200

    Create Basic_Kalkulator.py

---
+++ Basic_Kalkulator.py
@@ -0,0 +1,17 @@
+#Basic Kalkulator programa vo Python
+broj1=input("Vnesi go prviot broj: ") #vo broj1 ke go zacuvame inputot, tekstot e samo text koj se pecati na ekran
+broj2=input("Vnesi go vtoriot broj: ")
+rezultat=int(broj1)+int(broj2)
+print("Sobiranje: "+str(rezultat))
+
+rezultat=int(broj1)-int(broj2)
+print("Odzemanje: "+str(rezultat))
+
+rezultat=int(broj1)*int(broj2)
+print("Mnozenje: "+str(rezultat))
+
+rezultat=int(broj1)/int(broj2)
+print("Delenje: "+str(rezultat))
+
+rezultat=int(broj1)%int(broj2)
+print("Ostatok: "+str(rezultat))
