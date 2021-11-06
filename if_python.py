9f8ddd18d5c5bd218e869a66e92374d3c17d029d
janevnikola <72938546+janevnikola@users.noreply.github.com>
2021-11-06 15:59:21 +0100

    Create if_python.py

---
+++ if_python.py
@@ -0,0 +1,9 @@
+ime="Nikola"
+godini=45
+
+if ime=="Nikola" and str(godini)==str(45): #and e && t.e i, a or e || t.e ili
+    print("Ti se vikas: "+ime+" i imas "+str(godini))
+elif ime=="Tosko" and str(godini)==str(67):
+    print("Ti ne se vikas "+ime+" i nemas "+str(godini))
+else:
+    print("Vnesi drugo ime")
