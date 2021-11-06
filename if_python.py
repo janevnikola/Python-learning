a37173170092fb15606050bb21205db9b506cb6b
janevnikola <72938546+janevnikola@users.noreply.github.com>
2021-11-06 16:12:59 +0100

    Update if_python.py

--- if_python.py
+++ if_python.py
@@ -6,5 +6,5 @@ if ime=="Nikola" and str(godini)==str(45): #and e && t.e i, a or e || t.e ili
     print("Ti se vikas: "+ime+" i imas "+str(godini))
 elif ime=="Tosko" and str(godini)==str(67):
     print("Ti ne se vikas "+ime+" i nemas "+str(godini))
-elif not(daliEvisok): #!=
+elif not(daliEvisok): #ako daliEvisok e false
     print("Vnesi drugo ime")
