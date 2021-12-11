47da912ca51c974f45af5ac7b70406cae6303737
janevnikola <72938546+janevnikola@users.noreply.github.com>
2021-12-11 19:46:22 +0100

    Create FOR_loops.py

---
+++ FOR_loops.py
@@ -0,0 +1,35 @@
+string_niza=["Zdravo","Zbor dva","Zbor tri"]
+
+print("Najdobro pecatenje:")
+print("\nPecatenje na elementite no so niza[i]")
+for i in range(len(string_niza)):
+    print(string_niza[i])
+
+
+Zbor="Zdravo"
+print("Pecatenje za string")
+for i in Zbor:
+    print(i)
+
+
+
+print("\n")
+print("Pecatenje na indeksi vo niza vo niza")
+niza=[1,2,3,4,5,6,7,8,9,10,2,3,4,5,6]
+for i in range(len(niza)):
+    print(i)
+
+print("\n")
+print("Printanje in range")
+
+print("Ke gi ispecati 0 do 10")
+for i in range(10):
+    print(i)
+
+
+
+print("\nPecatenje za niza od strings")
+
+for i in string_niza:
+    print(i)
+
