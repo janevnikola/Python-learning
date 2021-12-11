7b1965bb14d07cd858c3db829f7ee787391b2154
janevnikola <72938546+janevnikola@users.noreply.github.com>
2021-12-11 19:58:09 +0100

    Update MATRICI.py

--- MATRICI.py
+++ MATRICI.py
@@ -1,7 +1,15 @@
-matrica=[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
+matrica = [
+    [1, 2, 3],  # 0
+    [4, 5, 6],  # 1
+    [7, 8, 9]  # 2
 ]
-#matrica [0] [1] znaci 0 redica, 1 kolona
-print(matrica[0][1]) #pecatenje na 2
+# matrica [0] [1] znaci 0 redica, 1 kolona
+print(matrica[0][1])  # pecatenje na 2
+print("\n")
+print("Obicen print")
+for matrica_indeks in matrica:
+    print(matrica_indeks)
+print("\n printanje na sekoj element vo matricata")
+for i in matrica:
+    for j in i:
+        print(j)
